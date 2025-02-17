7fea3f8b0ffecdeedf1703cef7d4c737b05e08ca
nusaibuhh <nususc19@gmail.com>
2025-02-18 01:53:34 +0600

    add files

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#features">Features</a></li>
-                <li><a href="#login">Log In</a></li>
+                <li><a href="login.php">Log In</a></li>
             </ul>
         </nav>
     </header>
